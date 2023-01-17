5d77e75a06a9d5ce631468aab42f6f359cef82c3
ccp84 <ccp84@icloud.com>
2023-01-17 14:54:30 +0000

    set up programmer table

--- sql-crud.py
+++ sql-crud.py
@@ -8,6 +8,18 @@ from sqlalchemy.orm import sessionmaker
 db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
+
+# create class based Programmer table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+
 # create a new instance of session maker class from the database link
 Session = sessionmaker(db)
 # open a session by calling the session subclass
