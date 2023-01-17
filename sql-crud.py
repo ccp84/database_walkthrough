3b905f8a0585ffa438b2652204773af61363b477
ccp84 <ccp84@icloud.com>
2023-01-17 20:38:23 +0000

    add delete function

--- sql-crud.py
+++ sql-crud.py
@@ -28,3 +28,117 @@ session = Session()
 
 # create database from declarative base subclass
 base.metadata.create_all(db)
+
+
+# create records
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First programmer",
+)
+
+
+alan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing",
+)
+
+
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL Language",
+)
+
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11",
+)
+
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft",
+)
+
+
+tim_berners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web",
+)
+
+
+# add instance to database
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+
+
+# programmer = session.query(Programmer).filter_by(id=2).first()
+# programmer.first_name = "Cheryl"
+# programmer.last_name = "Phillips"
+# programmer.famous_for = "Knitting"
+
+# # commit changes
+# session.commit()
+
+# update multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+fname = input("Enter a first name: ")
+lname = input("Enter a last name: ")
+programmer = session.query(Programmer).filter_by(
+    first_name=fname, last_name=lname).first()
+if programmer is not None:
+    print(
+        "Programmer found: ", programmer.first_name +
+        " " + programmer.last_name)
+    confirmation = input("Confirm deletion y/n: ")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
+        session.commit()
+    else:
+        print("Programmer not deleted")
+else:
+    print("No records found")
+
+
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
